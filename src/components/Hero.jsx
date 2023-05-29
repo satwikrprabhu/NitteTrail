21f94376401e6ee52a0bad014c91cab0f89a0750
satwikrprabhu <satwikprabhu@gmail.com>
2023-05-29 18:56:04 +0530

    feat:gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const NitteTrail = () => {
 
   return (
     <div className='h-screen bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r' style={{paddingTop:search?"10rem":"16rem"}}>
-       {search?"":<h1 className='text-7xl font-bold text-center text-white flex justify-center items-center'>NitteTrail</h1>}
+       {search?"":<h1 className='text-7xl font-bold text-center text-white flex justify-center items-center nittetext'>NitteTrail</h1>}
         <form className='xl:mx-[25rem] lg:mx-[20rem] md:mx-[15rem] sm:mx-[10rem] mx-8 mt-8'>   
     <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
     <div className="relative">
@@ -42,7 +42,7 @@ const NitteTrail = () => {
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
         <Search setSearch={(search)=>setSearch(search)}/>
-        <button type="submit" className=" text-black absolute right-2.5 bottom-2.5 bg-nitteYellow hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-nitteYellow dark:hover:bg-yellow-300 dark:focus:ring-red-800">Search</button>
+        <button type="submit" className=" text-black absolute right-2.5 bottom-2.5 bg-nitteYellow hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-semibold rounded-lg text-sm px-4 py-2 dark:bg-nitteYellow dark:hover:bg-yellow-300 dark:focus:ring-red-800 nittetext1">Search</button>
     </div>
 </form>
 <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,12 +20,12 @@ const links = [{
   return (
     <nav className='fixed w-full top-0 z-20  border-b border-gray-400/20 bg-white bg-opacity-5 backdrop-blur-lg backdrop-filter'>
       <div className='flex justify-between items-center mx-auto max-w-5xl h-16'>
-      <h1 className='text-2xl ml-6 font-bold text-white cursor-pointer'>
+      <h1 className='text-2xl ml-6 font-bold text-white cursor-pointer nittetext'>
         NitteTrail
       </h1>
       <ul className='flex justify-between items-center space-x-10 font-semibold'>
         {links.map((link)=>(<li className='hidden lg:block text-lg text-white hover:text-gray-600 cursor-pointer'>{link.linkname}</li>))}
-        <button className='hidden lg:block text-lg mr-8 bg-nitteYellow hover:bg-yellow-300 text-black font-semi-bold py-1 px-4 rounded'>Login</button>
+        <button className='hidden lg:block text-lg mr-8 bg-nitteYellow hover:bg-yellow-300 text-black font-semi-bold py-1 px-4 rounded nittetext1'>Login</button>
       </ul></div>
       
     </nav>
