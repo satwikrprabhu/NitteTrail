758cb10c223bec1c84f79f8caf6b76242ebc65b2
satwikrprabhu <satwikprabhu@gmail.com>
2023-05-29 23:21:50 +0530

    fix:logo icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,14 @@ const links = [{
 
   return (
     <nav className='fixed w-full top-0 z-20  border-b border-gray-400/20 bg-white bg-opacity-5 backdrop-blur-lg backdrop-filter'>
-      <div className='flex justify-between items-center mx-auto max-w-5xl h-16'>
-      <h1 className='text-2xl ml-6 font-bold text-white cursor-pointer nittetext'>
+      <div className='flex justify-between items-center mx-auto max-w-5xl h-16'><div className='flex items-center ml-5'>
+        <img src="./nittelogotrans.png" alt="" width="45px"/>
+      <h1 className='hidden md:block text-2xl ml-2 font-bold text-white cursor-pointer'>
         NitteTrail
       </h1>
+        
+      </div>
+      
       <ul className='flex justify-between items-center space-x-10 font-semibold'>
         {links.map((link)=>(<li className='hidden lg:block text-lg text-white hover:text-gray-300 cursor-pointer'>{link.linkname}</li>))}
         <button className='hidden lg:block text-lg mr-8 bg-white hover:bg-gray-200 text-black font-semi-bold py-1 px-4 rounded '>Login</button>
