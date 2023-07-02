ed8d40b6da7ef1c004fa7655c5369d9dc34a896a
Satwik Prabhu <satwikprabhu@gmail.com>
2023-07-02 15:45:31 +0530

    fix: unnecessary files deleted

--- src/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const {Router:expressRouter} = require("express");
-
-const router = expressRouter();
-
-const authRouter = require("./authRoutes");
-router.use("/auth",authRouter);
-module.exports = router;
-
-
