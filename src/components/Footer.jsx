8cb6e921af05f51651a89e7a58a0c4ccad93f0d3
satwikrprabhu <satwikprabhu@gmail.com>
2023-07-02 15:39:43 +0530

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
     <footer className='bg-gray-900  font-Poppins'>
     <div className='text-center w-full py-8 font-semibold bottom-0'>
       <p className="text-white mb-6">&copy;NitteTrail</p>
-      <p className="text-white mb-6 text-center">Made with ❤️ by Satwik Prabhu</p>
+      <p className="text-white mb-6 text-center">Made with ❤️ by Team Techtonics</p>
       <ul className='flex items-center justify-center space-x-20  text-white'>
         {links.map((link)=><li>{link.linkname}</li>)}
       </ul>
